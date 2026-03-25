1d9f12bc577ef168a457b59ed7f7838cd5dbec30
konstantinseym <konstantinseym@proton.me>
2026-03-25 22:08:33 +0300

    feat: scroll to top button
    animated "scroll to top" button works with fade-in, fade-out, hover (scale, opacity) animation.

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,4 +1,7 @@
 import styles from "./MainLayout.module.css";
+import { createPortal } from "react-dom";
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "motion/react";
 import Caption from "../UI/Caption/Caption.jsx";
 import Announcements from "../Announcements/Announcements.jsx";
 import Employees from "../Employees/Employees.jsx";
@@ -6,24 +9,63 @@ import Details from "../Details/Details.jsx";
 import Contacts from "../Contacts/Contacts.jsx";
 
 export default function MainLayout() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  function scrollToTop() {
+    window.scrollTo(0, 0);
+  }
+
+  useEffect(() => {
+    function handleScroll() {
+      setIsScrolled(window.scrollY > 50);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <main className={styles.mainlayout}>
-      <div className={styles.mainlayout__block}>
-        <Caption>Объявления</Caption>
-        <Announcements />
-      </div>
-      <div className={styles.mainlayout__block}>
-        <Caption>Адвокаты</Caption>
-        <Employees />
-      </div>
-      <div className={styles.mainlayout__block}>
-        <Caption>Реквизиты</Caption>
-        <Details />
-      </div>
-      <div className={styles.mainlayout__block}>
-        <Caption>Контакты</Caption>
-        <Contacts />
-      </div>
-    </main>
+    <>
+      <main className={styles.mainlayout}>
+        <div className={styles.mainlayout__block}>
+          <Caption>Объявления</Caption>
+          <Announcements />
+        </div>
+        <div className={styles.mainlayout__block}>
+          <Caption>Адвокаты</Caption>
+          <Employees />
+        </div>
+        <div className={styles.mainlayout__block}>
+          <Caption>Реквизиты</Caption>
+          <Details />
+        </div>
+        <div className={styles.mainlayout__block}>
+          <Caption>Контакты</Caption>
+          <Contacts />
+        </div>
+      </main>
+
+      {createPortal(
+        <AnimatePresence>
+          {isScrolled && (
+            <motion.svg
+              initial={{ opacity: 0, filter: "blur(10px)" }}
+              animate={{ opacity: 1, filter: "blur(0px)" }}
+              exit={{ opacity: 0, filter: "blur(10px)" }}
+              transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
+              className={styles.mainlayout__arrowupbtn}
+              viewBox="0 0 38 19"
+              onClick={scrollToTop}
+            >
+              <path d="M19.535,0c0.241,0,0.483,0.087,0.673,0.261L38.416,16.852c0.408,0.372,0.438,1.004,0.066,1.413c-0.372,0.409-1.004,0.439-1.413,0.066L19.535,2.352L2,18.331c-0.411,0.372-1.042,0.342-1.413-0.066c-0.372-0.408-0.343-1.041,0.065-1.413L18.862,0.26C19.052,0.087,19.294,0,19.535,0z" />
+            </motion.svg>
+          )}
+        </AnimatePresence>,
+        document.body,
+      )}
+    </>
   );
 }
