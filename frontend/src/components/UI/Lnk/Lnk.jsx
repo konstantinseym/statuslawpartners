f464e92644446f67f37ec1913a94223560d02c4c
konstantinseym <konstantinseym@outlook.com>
2026-04-25 18:33:47 +0500

    fix: fixed problems with employees cards scaling

--- frontend/src/components/UI/Lnk/Lnk.jsx
+++ frontend/src/components/UI/Lnk/Lnk.jsx
@@ -2,7 +2,13 @@ import styles from "./Lnk.module.css";
 
 export default function Lnk({ children, style, path }) {
   return (
-    <a className={styles.lnk} style={style} href={path}>
+    <a
+      className={styles.lnk}
+      style={style}
+      href={path}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       {children}
     </a>
   );
