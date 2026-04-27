0b6ebd19bbcc2ccdd359272ebad618dd281ab8b5
konstantinseym <konstantinseym@outlook.com>
2026-04-27 20:18:06 +0500

    fix: updatePolicy.js wrong request fixed

--- frontend/src/components/ControlPanel/api/updatePolicy.js
+++ frontend/src/components/ControlPanel/api/updatePolicy.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function updatePolicy(policyData) {
   try {
-    const response = await axios.put("/api/hero-background", policyData);
+    const response = await axios.put("/api/policy", policyData);
     return response;
   } catch (err) {
     console.log(err);
